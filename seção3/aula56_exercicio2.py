196eb16e5e0015331043c87fef650497cc211e04
Yasmin Mota <124382453+yaasminmota@users.noreply.github.com>
2024-07-02 20:26:11 -0300

    Atualizada em 02/07/2024

--- seção3/aula56_exercicio2.py
+++ seção3/aula56_exercicio2.py
@@ -3,3 +3,20 @@ Faça um programa que avalia a hora ao usuário e, baseando-se no horário
 descrito, exiba o certificado solicitado. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
+try:
+    hora = int(input('Digite as horas em números inteiros: '))
+
+    if hora >= 0 and hora <=11:
+        print('Bom dia')
+    elif hora >= 12 and hora <= 17:
+        print('Boa Tarde')
+    elif hora >= 18 and hora <= 23:
+        print('Boa noite')
+
+    else:
+        print('Não conheço essa hora')
+
+except:
+    print('Você nao digitou um numero inteiro.')
+
+
--- seção3/aula57_exercicio3.py
+++ seção3/aula57_exercicio3.py
@@ -3,3 +3,12 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 menos Escrever "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande".
 """
+
+nome = len(input('Digite seu nome:'))
+
+if nome <= 4:
+    print('O nome é curto')
+if nome == 5 or nome ==6:
+    print('Seu nome é normal')
+if nome > 6:
+     print('Seu nome é muito grande')
